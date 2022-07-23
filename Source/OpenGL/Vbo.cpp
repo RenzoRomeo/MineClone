10f8ed1f9e7aeb50dc4f2de330f5fc888f2f0c08
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-23 02:26:23 -0300

    Dynamic vbo draw

--- Source/OpenGL/Vbo.cpp
+++ Source/OpenGL/Vbo.cpp
@@ -4,7 +4,7 @@ Vbo::Vbo(const void* data, uint32_t size)
 {
 	glGenBuffers(1, &m_id);
 	glBindBuffer(GL_ARRAY_BUFFER, m_id);
-	glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, size, data, GL_DYNAMIC_DRAW);
 }
 
 Vbo::~Vbo()
