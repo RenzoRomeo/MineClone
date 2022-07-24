1a8beffde3c343d7400b286ea86fa7e04ecf0fe0
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-24 19:00:10 -0300

    Texture parameters

--- Source/OpenGL/Texture.cpp
+++ Source/OpenGL/Texture.cpp
@@ -16,10 +16,12 @@ Texture::Texture(const std::string& imageFilepath)
 	glGenTextures(1, &m_id);
 	glBindTexture(GL_TEXTURE_2D, m_id);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
 	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, m_width, m_height, 0, textureFormat, GL_UNSIGNED_BYTE, data);
 
 	stbi_image_free(data);
