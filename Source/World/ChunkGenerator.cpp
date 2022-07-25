92225c26686525ac687f50ffde6884eea5c5b4a8
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-24 22:02:21 -0300

    Fixed terrain generation!

--- Source/World/ChunkGenerator.cpp
+++ Source/World/ChunkGenerator.cpp
@@ -20,11 +20,12 @@ namespace ChunkGenerator
 
 		// Higher scaling factor -> more "aggressive" terrain.
 		const float noiseScaling = 2.0f;
+
 		for (size_t x = 0; x < Chunk::horizontalSize; x++)
 		{
 			for (size_t z = 0; z < Chunk::horizontalSize; z++)
 			{
-				glm::vec2 absoluteBlockPosition{ ((float)position.x + x), ((float)position.y + z) };
+				glm::vec2 absoluteBlockPosition{ ((float)position.x * (float)Chunk::horizontalSize + x), ((float)position.y * (float)Chunk::horizontalSize + z)};
 				absoluteBlockPosition.x = map(absoluteBlockPosition.x, 0.0f, (float)(World::worldSize * Chunk::horizontalSize), -1.0f, 1.0f);
 				absoluteBlockPosition.y = map(absoluteBlockPosition.y, 0.0f, (float)(World::worldSize * Chunk::horizontalSize), -1.0f, 1.0f);
 				float noise = glm::simplex(absoluteBlockPosition * noiseScaling);
@@ -34,7 +35,11 @@ namespace ChunkGenerator
 					if (y > height)
 						blocks->at(x).at(y).at(z) = Block{ BlockType::Air, false };
 					else
-						blocks->at(x).at(y).at(z) = Block{ BlockType::Wood, true };
+					{
+						if (y <= 2) blocks->at(x).at(y).at(z) = Block{ BlockType::Stone, true };
+						else blocks->at(x).at(y).at(z) = Block{ BlockType::Grass, true };
+					}
+						
 				}
 			}
 		}
