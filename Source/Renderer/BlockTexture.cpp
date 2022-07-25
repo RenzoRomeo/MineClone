a67b9f56188d0898aa7923699ec8196b9a7c633a
Renzo Romeo <renzoromeo44@gmail.com>
2022-07-24 22:01:34 -0300

    New blocks

--- Source/Renderer/BlockTexture.cpp
+++ Source/Renderer/BlockTexture.cpp
@@ -4,11 +4,16 @@ namespace BlockTexture
 {
 	glm::ivec2 getBlockAtlas(const Block& block, Sides side)
 	{
-		if (block.type == BlockType::Dirt)
+		if (block.type == BlockType::Grass)
 		{
 			if (side == Sides::YP) return { 0, 0 };
 			if (side == Sides::YN) return { 2, 0 };
 			return { 1, 0 };
+		}
+
+		if (block.type == BlockType::Dirt)
+		{
+			return { 2, 0 };
 		}
 
 		if (block.type == BlockType::Wood)
@@ -16,6 +21,16 @@ namespace BlockTexture
 			if (side == Sides::YP) return { 3, 1 };
 			if (side == Sides::YN) return { 3, 1 };
 			return { 2, 1 };
+		}
+
+		if (block.type == BlockType::Planks)
+		{
+			return { 6, 1 };
+		}
+
+		if (block.type == BlockType::Stone)
+		{
+			return { 3, 0 };
 		}
 	}
 }
